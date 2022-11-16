81ea36d354c818bc4798548f3be6a42ffb25c12e
sasidharavarmaconcentrix <109135486+sasidharavarmaconcentrix@users.noreply.github.com>
2022-11-16 11:14:55 +0530

    Changed to RestController upto Add Functionality Completed

--- Global_Exchange_Centre/src/main/java/com/example/demo/GlobalExchangeCentreApplication.java
+++ Global_Exchange_Centre/src/main/java/com/example/demo/GlobalExchangeCentreApplication.java
@@ -28,19 +28,19 @@ public class GlobalExchangeCentreApplication {
 	    
 	//CompanyShareRepository repo = applicationcontext.getBean(CompanyShareRepository.class);
 		 GEC_Service_Impl gec_service =applicationcontext.getBean(GEC_Service_Impl.class);
-		 gec_service.getListOfCpmpanies(); 
-		 CompanyShare new_company=new CompanyShare(); 
-		 new_company.setCompanyId((long) 10000000);
-		 new_company.setCompanyName("concentrix"); 
-		 new_company.setSharePrice(100.00);
-		 gec_service.saveCompany(new_company);
-		 //System.out.println(gec_service.getListOfCpmpanies());
-		 CompanyShare new_company1=new CompanyShare(); 
-		 new_company1.setCompanyId((long) 10000001);
-		 new_company1.setCompanyName("synnex"); 
-		 new_company1.setSharePrice(101.00);
-		 gec_service.saveCompany(new_company1);
-		 gec_service.fetchcompanybyid(10000001);
+//		 gec_service.getListOfCpmpanies(); 
+//		 CompanyShare new_company=new CompanyShare(); 
+//		 new_company.setCompanyId((long) 10000000);
+//		 new_company.setCompanyName("concentrix"); 
+//		 new_company.setSharePrice(100.00);
+//		 gec_service.saveCompany(new_company);
+//		 //System.out.println(gec_service.getListOfCpmpanies());
+//		 CompanyShare new_company1=new CompanyShare(); 
+//		 new_company1.setCompanyId((long) 10000001);
+//		 new_company1.setCompanyName("synnex"); 
+//		 new_company1.setSharePrice(101.00);
+//		 gec_service.saveCompany(new_company1);
+//		 gec_service.fetchcompanybyid(10000001);
 		 //System.out.println(gec_service.getListOfCpmpanies());
 		// gec_service.deleteemployeebyid(10000001);
 		 
--- Global_Exchange_Centre/src/main/java/com/example/demo/controller/ShareController.java
+++ Global_Exchange_Centre/src/main/java/com/example/demo/controller/ShareController.java
@@ -6,43 +6,44 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.model.CompanyShare;
 import com.example.demo.service.GEC_Service;
 import com.example.demo.service.GEC_Service_Impl;
 
-@Controller
+@RestController
 public class ShareController {
     @Autowired
     private GEC_Service gec_service;
     @GetMapping("/")
-    public String viewlistofcompanies(Model model) {
-    	model.addAttribute("listofcompanies",gec_service.getListOfCpmpanies());
+    public ModelAndView viewlistofcompanies(Model model) {
+    	ModelAndView modelAndView = new ModelAndView("/home_companies_list"); 
+    	modelAndView.addObject("listofcompanies",gec_service.getListOfCpmpanies());
+    	return modelAndView;
     	
+    	//model.addAttribute("listofcompanies",gec_service.getListOfCpmpanies());
     	//System.out.println("ji");
-		return "home_companies_list";
+		//return "home_companies_list";    	
+    	
     }
     @GetMapping("/AddCompany")
-    public String AddCompanytoList(Model model) {
+    public ModelAndView AddCompanytoList(Model model) {
 		//System.out.println("Add te company");
-    	return "Add_new_Company_form";
+    	ModelAndView modelAndView = new ModelAndView("Add_new_Company_form"); 
+    	return modelAndView;
     	
     }
     @PostMapping("/add_new_company_toList")
-    public String doTransfer(
+    public ModelAndView doAdd(
             @RequestParam("CompanyID") long CompanyID,
             @RequestParam("CompanyName") String CompanyName,
             @RequestParam("SharePrice") Double SharePrice) {
             
         CompanyShare companyshare=new CompanyShare(CompanyID,CompanyName,SharePrice);
-		/*
-		 * gec_service.saveCompany(companyshare); ModelAndView modelAndView = new
-		 * ModelAndView(); modelAndView.setViewName("home_companies_list"); return
-		 * modelAndView;
-		 */
         gec_service.saveCompany(companyshare);
-      //  return "redirect:/home_companies_list";
-        return "redirect:/";
+        ModelAndView modelAndView = new ModelAndView("redirect:/"); 
+        return modelAndView;
     }
 }
